e98de653feef5f02dd7c3f79ce1676c727668b01
Manav Lade <109347337+manavlade@users.noreply.github.com>
2023-10-11 20:28:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,7 @@ import { getFirestore } from "@firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGfUnonODTqoFn0Iimf2UTeFE-dj2lb-g",
-  authDomain: "beast-chat-66bda.firebaseapp.com",
-  projectId: "beast-chat-66bda",
-  storageBucket: "beast-chat-66bda.appspot.com",
-  messagingSenderId: "771220849014",
-  appId: "1:771220849014:web:afcb5ecef408d9be0e3a57",
-  measurementId: "G-6TME0W8S84",
-  databaseURL: "https://beast-chat-66bda-default-rtdb.firebaseio.com/",
+//YOUR CONFIG DETAILS OF FIREBASE
 };
 
 // Initialize Firebase
